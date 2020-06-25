55d163691a433a18c871ced3f7d91a1192d7fcc8
quanbn1703 <61594116+quanbn1703@users.noreply.github.com>
2020-06-25 09:34:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" type="text/css" href="">
     <style>
         html,body{
-            background-image: url('http://getwallpapers.com/wallpaper/full/a/5/d/544750.jpg');
+            
             background-size: cover;
             background-repeat: no-repeat;
             height: 100%;
